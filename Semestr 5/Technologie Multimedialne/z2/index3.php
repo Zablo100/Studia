9960b858a56940023515563f618c61a62c3cd29b
Zablo100 <ZabloEmail@gmail.com>
2022-11-05 09:01:06 +0100

    [TM] Lab2

---
+++ Semestr 5/Technologie Multimedialne/z2/index3.php
@@ -0,0 +1,16 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+    <title>Zabłocki</title>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<BODY>
+Formularz logowania
+<form method="post" action="weryfikuj3.php">
+ Login:<input type="text" name="user" maxlength="20" size="20" autocomplete="off"><br>
+ Hasło:<input type="password" name="pass" maxlength="20" size="20"><br>
+ <input type="submit" value="Send"/>
+</form>
+
+</BODY>
+</HTML>
---
+++ Semestr 5/Technologie Multimedialne/z2/logout.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1); // włączenie typowania zmiennych w PHP >=7
+session_start();
+if (!isset($_SESSION['loggedin']))
+{
+header('Location: index3.php');
+exit();
+}
+session_destroy();
+include("index3.php")
+?>
